c90e2c4dd014cc06d38b0ea41b378322e76e23b3
gautam777777 <gautamsharma0057@gmail.com>
2023-06-15 09:10:47 +0530

    function

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Functions
+
+//1.Function Defination Area (one time process)
+function getMyname(){  //Not a camelCaseLetter
+    console.log("Gautam"); //js statement
+}
+
+function getMySurname(){  //camelCaseLetter
+    //Function body
+    //Inside the function body I can write many Statement
+    console.log("Sharma"); //js statement
+}
+
+//2.Function Calling/Invoking Area
+getMyname();
+getMySurname();
---
+++ js/script2.js
@@ -0,0 +1,17 @@
+
+//Let's create a function to add a two number
+
+//1.Function Defination Area (one time process)
+
+function addTwoNumbers(n1,n2){  //camelCaseLetter //n1,n2 are formal Argument
+    //Function body
+    //Inside the function body I can write many Statement
+    //Every function return something
+    return n1 + n2;
+    
+}
+
+//2.Function Calling/Invoking Area (many time process)
+
+var result = addTwoNumbers(10,30);  //Actual Arguments
+console.log(result);
